726bd5691ec12aab48e5bde48f4b14c5d8301e1b
ilyas <nazih.azerty@gmail.com>
2024-05-21 13:54:07 +0100

    Frontend

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from "../components/Hero";
+import Biography from "../components/Biography";
+import Departments from "../components/Departments";
+import MessageForm from "../components/MessageForm";
+
+const Home = () => {
+   return (
+    <>
+       <Hero title={"Welcome to NzCaare Medical Institute | Your Trusted Healthcare provider"} imageUrl={"/hero.png"}/>
+       <Biography imageUrl={"/about.png"}/>
+       <Departments/>
+       <MessageForm/>
+   </>
+   );
+};
+
+export default Home
